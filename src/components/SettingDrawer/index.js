7ae4dd0cc0782b4b8d05731b92740c596a4ae5d6
bsj <875848407@qq.com>
2019-09-27 19:15:27 +0800

    update

--- src/components/SettingDrawer/index.js
+++ src/components/SettingDrawer/index.js
@@ -1,15 +1,40 @@
 import React, { PureComponent } from 'react';
-import {
-  Select,
-  message,
-  Drawer,
-  List,
-  Switch,
-  Divider,
-  Icon,
-  Button,
-  Alert,
-  Tooltip
-} from 'antd';
-import styles from './style.less';
-import ThemeColor from './ThemeColor';
+// import {
+//   Select,
+//   message,
+//   Drawer,
+//   List,
+//   Switch,
+//   Divider,
+//   Icon,
+//   Button,
+//   Alert,
+//   Tooltip
+// } from 'antd';
+// import styles from './style.less';
+// import ThemeColor from './ThemeColor';
+// import BlockCheckbox from './BlockCheckbox';
+
+// const Body = ({ children, title, style }) => (
+//   <div
+//     style={{
+//       ...style,
+//       marginBottom: 24
+//     }}
+//   >
+//     <h3 className={styles.title}>{title}</h3>
+//     {children}
+//   </div>
+// );
+
+class SettingDrawer extends PureComponent {
+  state = {
+    collapse: false
+  };
+
+  render() {
+    return <div>SettingDrawer</div>;
+  }
+}
+
+export default SettingDrawer;
