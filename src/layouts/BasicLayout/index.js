989dc00c4ae537658797efd4e2149e851d2f5914
bsj <875848407@qq.com>
2019-09-30 18:39:24 +0800

    update

---
+++ src/layouts/BasicLayout/index.js
@@ -0,0 +1,17 @@
+import React, { Suspense } from 'react';
+import { Layout } from 'antd';
+import DocumentTitle from 'react-document-title';
+import isEqual from 'lodash/isEqual';
+import memoizeOne from 'memoize-one';
+import { ContainerQuery } from 'react-container-query';
+import classNames from 'classnames';
+import pathToRegexp from 'path-to-regexp';
+import Media from 'react-media';
+
+class BasicLayout extends React.PureComponent {
+  render() {
+    return <div>BasicLayout</div>;
+  }
+}
+
+export default BasicLayout;
